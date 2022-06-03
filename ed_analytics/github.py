3c80cf5b54775f92364fe192dff8c81a8de500ca
Shreyas <shreyass.ranganatha@gmail.com>
2022-06-03 18:04:38 +0530

    Add get_commits()

--- ed_analytics/github.py
+++ ed_analytics/github.py
@@ -1,5 +1,7 @@
 
 from typing import Sequence
+import requests
+
 from ed_analytics.abc import Commit
 
 
@@ -36,3 +38,31 @@ class Repository:
         ----------
         https://docs.github.com/en/rest/commits/commits#list-commits--parameters
         """
+
+        params = {
+            "author": author,
+            "since": since,
+            "per_page": per_page,
+            "until": until
+        }
+
+        for i in range(*(
+                (1, 10+1) if page is None 
+                else (page, page+1)
+            )):
+
+            params["page"] = i
+            
+            rq = requests.get(
+                    "https://api.github.com/repos/{}/{}/commits".format(self.owner, self.reponame),
+                    params={
+                        k: v
+                        for k, v in params.items() if v is not None
+                    },
+                    headers={"Authorization": f"token {self.__oauth_token}"} if self.__oauth_token else None
+                )
+
+            if not rq.json():
+                return
+
+            yield [Commit(cmt) for cmt in rq.json()]
