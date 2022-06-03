2d1b0299bd2fe05b073578156df5d2c29875bc8a
hitaish kg <91547035+Hitaishkg@users.noreply.github.com>
2022-06-03 14:16:38 +0000

    program getcommits()

--- ed_analytics/github.py
+++ ed_analytics/github.py
@@ -1,9 +1,10 @@
-
+#  https://api.github.com/repos/OWNER/REPO/commits
 import requests
 import json
 
+
+
 from typing import Sequence
-from ed_analytics.abc import Commit
 
 
 class Repository:
@@ -16,30 +17,15 @@ class Repository:
     def authenticate(self, oauth_token):
         self.__oauth_token: str = oauth_token
 
-    def get_commits(self, author: str = None, since: str = None, per_page: int = None, page: int = None, until: str = None) -> Sequence[Commit]:
-        """
-        Parameters
-        ----------
-        author : str
-            query parameter of commit author
-
-        since : str
-            since timestamp in ISO 8601 format YYYY-MM-DDTHH:MM:SSZ
+    
+    def get_commits(self, author: str = None, since: str = None, per_page: int = None, page: int = None, until: str = None):
         
-        per_page : int
-            number of responses per page
+        
+        
+        
+        url=" https://api.github.com/repos/{}/{}/commits".format(self.owner,self.reponame)
 
-        page : int
-            page of output
-
-        until : str
-            until timestamp in ISO 8601 format YYYY-MM-DDTHH:MM:SSZ
-
-        References
-        ----------
-        https://docs.github.com/en/rest/commits/commits#list-commits--parameters
-        """
-      res={
+        res={
             'author':author,
             'since':since,
             'per_page':per_page,
@@ -51,11 +37,6 @@ class Repository:
             h:r
             for h,r in res.items() 
             if r is not None
-            })
+            }).json()
                     
-                
-
-        x=json.loads(r.text)
-        print(json.dumps(x ,indent=2))
-rp = Repository("skylartown", "ed-analytics.py")
-print(rp.get_commits())
+        return r
